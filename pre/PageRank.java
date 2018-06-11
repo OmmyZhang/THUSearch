4f7e15095dc455b083230b744587bcbaee7b002d
ommyzhang <tdxdxoz@gmail.com>
2018-06-11 18:13:13 +0800

    完成了只处理站内链接的构图，和PageRank计算

--- pre/PageRank.java
+++ pre/PageRank.java
@@ -5,24 +5,20 @@ import java.util.regex.*;
 
 public class PageRank
 {
-    private static final String base = "data";
-    private static String site;
-    private static Map<String, WebNode> nodeMap;
+    private static final String base_data_dir = "data";
+	private static String base; // current base path
+    private static String site; // current site
+	private static Set<String> sites; // all sites
+
+	private static final String slash = "/"; // NOTE: may change to \ in Windows
+
     
-    private static void link(WebNode cNode,WebNode  tNode)
-    {
-        cNode.d_out++;
-        tNode.d_in++;
-    }
-    
+
     private static void deal_file(File f, String path) throws Exception
     {
-        System.out.println(path);
+        //System.out.println(path);
         
-        WebNode curNode;
-        if(!nodeMap.containsKey(site+path))
-            nodeMap.put(site+path, new WebNode(site, path));
-        curNode = nodeMap.get(site+path);
+        WebNode curNode = WebNode.get_or_create(site, path);
         
         Scanner scan = new Scanner(f);
         Pattern p = Pattern.compile("[\"\'](\\/.*?\\.html)[\"\']");
@@ -34,14 +30,10 @@ public class PageRank
             while(m.find())
             {
                 String go_in_site_path = m.group(1);
-                System.out.println(" : --> " + go_in_site_path);
+                //System.out.println(" : --> " + go_in_site_path);
                 
-                WebNode targetNode;
-                if(!nodeMap.containsKey(site + go_in_site_path))
-                    nodeMap.put(site + go_in_site_path, new WebNode(site, go_in_site_path));
-                targetNode = nodeMap.get(site+go_in_site_path);
-                
-                link(curNode, targetNode);
+                WebNode targetNode = WebNode.get_or_create(site, go_in_site_path);
+                WebEdge.link(curNode, targetNode);
             }
         }
         scan.close();
@@ -55,23 +47,68 @@ public class PageRank
         {
             File f = new File(currDir.getPath(), fname);
             if(f.isDirectory())
-                make_graph(path+"/"+fname);
+                make_graph(path + slash + fname);
             else
                 if(fname.endsWith(".html"))
-                    deal_file(f, path+"/"+fname);
+                    deal_file(f, path + slash + fname);
         }
         
     }
 
+	private static void calc_pr()
+	{
+		final double alpha = 0.15;
+		final int TN = 30; //  How many times when calc PageRank
+
+		int n = WebNode.nodeMap.size();
+		for(WebNode wn:WebNode.nodeMap.values())
+		{
+			wn.pr = 1.0 / n;
+		}
+
+		for(int k=1; k< TN; ++k)
+		{
+			double free = 0;
+			for(WebNode wn:WebNode.nodeMap.values())
+			{
+				wn.old_pr = wn.pr;
+				wn.pr = alpha / n;
+
+				if(wn.d_out == 0)
+					free += wn.old_pr;
+			}
+
+			for(WebEdge we:WebEdge.eList)
+				we.to.pr += (1 - alpha) * we.from.old_pr / we.from.d_out;
+
+			for(WebNode wn:WebNode.nodeMap.values())
+				wn.pr += (1 - alpha) * free / n;
+		}
+	}
+
     public static void main(String[] args) throws Exception
     {
-        nodeMap = new HashMap<String,WebNode>();
-        site = "math.tsinghua.edu.cn";
-        make_graph("");
+		File rootDir = new File(base_data_dir);
+
+		sites = new HashSet<String>(Arrays.asList(rootDir.list()));
+		for(String fname:sites)
+		{
+			if( ! new File(base_data_dir + slash + fname).isDirectory())
+				continue;
+			site = fname;
+			System.out.println("site: " + site);
+			base = base_data_dir + slash + site;
+			WebNode.init_map();
+			make_graph("");
+		}
+
+		// save if need
+
+		calc_pr();
         
-        for(WebNode wn:nodeMap.values())
+        for(WebNode wn:WebNode.nodeMap.values())
         {
-            System.out.println(wn.d_in + " " + wn.d_out + " " + wn.path);
+            System.out.println(String.format("%.6f %d %d %s",wn.pr,wn.d_in,wn.d_out,wn.path));
         }
     }
 }
@@ -79,10 +116,19 @@ public class PageRank
 class WebEdge
 {
     public WebNode from,to;
+	public static List<WebEdge> eList = new ArrayList<WebEdge>(); 
     
     public WebEdge(WebNode f, WebNode t)
     {
         from = f;
         to = t;
+    }
+    
+	protected static void link(WebNode f,WebNode  t)
+    {
+        f.d_out++;
+        t.d_in++;
+		eList.add(new WebEdge(f,t));
+
     }
 }
--- pre/WebNode.java
+++ pre/WebNode.java
@@ -1,10 +1,27 @@
+import java.util.*;
 
 class WebNode
 {
     public String site;
     public String path;
     public int d_in,d_out;
-    public float pr; //PageRank
+    public double pr, old_pr; //PageRank
+
+	// more var for Index
+
+    public static Map<String, WebNode> nodeMap;
+	public static WebNode get_or_create(String site, String path)
+	{
+		WebNode wn = nodeMap.get(site+path);
+        if(wn == null)
+            nodeMap.put(site+path, wn = new WebNode(site, path));
+			
+		return wn;
+	}
+	public static void init_map()
+	{
+        nodeMap = new HashMap<String,WebNode>();
+	}
  
     public WebNode(String s, String p)
     {
